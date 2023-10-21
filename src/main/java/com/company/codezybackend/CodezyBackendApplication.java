0cdee522f43869b75bdf3677c09b80c0387e04ca
AliHsynvv <gsynvali@gmail.com>
2023-10-21 09:17:22 -0700

    Initial commit

---
+++ src/main/java/com/company/codezybackend/CodezyBackendApplication.java
@@ -0,0 +1,13 @@
+package com.company.codezybackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodezyBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CodezyBackendApplication.class, args);
+    }
+
+}
